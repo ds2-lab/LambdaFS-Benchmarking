599c71857ec6a59e64d1d10ed409b64fe9013b30
Scusemua <benrcarver@gmail.com>
2021-04-24 14:58:11 -0400

    Added default constructor to HopsFSClient class.

--- src/main/java/com/gmail/benrcarver/HopsFSClient.java
+++ src/main/java/com/gmail/benrcarver/HopsFSClient.java
@@ -20,32 +20,32 @@ public class HopsFSClient implements Callable<BenchmarkResult>{
     /**
      * The number of RPC calls to make.
      */
-    private final int numRPC;
+    private int numRPC;
 
     /**
      * Current HopsFS API requires an ID, which is the ID of the desired user in the NDB database.
      */
-    private final int id;
+    private int id;
 
     /**
      * The URI of the NameNode we'll be contacting.
      */
-    private final String nameNodeUri;
+    private String nameNodeUri;
 
     /**
      * The query to be issued via the RPC call.
      */
-    private final String query;
+    private String query;
 
     /**
      * Indicates whether the NameNode should retrieve requested "metadata" from NDB or its local storage.
      */
-    private final String dataSource;
+    private String dataSource;
 
     /**
      * The NDB URI that the NameNode should issue NDB requests to.
      */
-    private final String ndbConnectionUri;
+    private String ndbConnectionUri;
 
     /**
      * The number of queries the NameNode should execute PER RPC REQUEST.
@@ -57,7 +57,7 @@ public class HopsFSClient implements Callable<BenchmarkResult>{
      * assuming the NameNode is directed to issue NDB queries rather than retrieve
      * data from its local memory/cache).
      */
-    private final int numQueries;
+    private int numQueries;
 
     public HopsFSClient(int numRpc, int numQueries, int id, String nameNodeUri, String query,
                         String dataSource, String ndbConnectionUri) {
@@ -68,6 +68,13 @@ public class HopsFSClient implements Callable<BenchmarkResult>{
         this.query = query;
         this.dataSource = dataSource;
         this.ndbConnectionUri = ndbConnectionUri;
+    }
+
+    /**
+     * Default constructor.
+     */
+    public HopsFSClient() {
+        
     }
 
     @Override
