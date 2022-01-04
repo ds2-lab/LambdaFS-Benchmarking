15e0e80ffbab13719773cff403b7ccc82042e184
Scusemua <benrcarver@gmail.com>
2022-01-03 19:02:27 -0500

    Added back the TreeNode class.

---
+++ src/main/java/com/gmail/benrcarver/TreeNode.java
@@ -0,0 +1,47 @@
+package com.gmail.benrcarver;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+public class TreeNode {
+    final String path;
+    final List<TreeNode> children;
+
+    public TreeNode(String path, List<TreeNode> children) {
+        this.path = path;
+        this.children = children;
+    }
+
+    public void addChildren(Collection<TreeNode> nodes) {
+        this.children.addAll(nodes);
+    }
+
+    public void addChild(TreeNode node) {
+        this.children.add(node);
+    }
+
+    public String getPath() {
+        return this.path;
+    }
+
+    public String toString() {
+        StringBuilder buffer = new StringBuilder(100);
+        print(buffer, "", "");
+        return buffer.toString();
+    }
+
+    private void print(StringBuilder buffer, String prefix, String childrenPrefix) {
+        buffer.append(prefix);
+        buffer.append(path);
+        buffer.append('\n');
+        for (Iterator<TreeNode> it = children.iterator(); it.hasNext();) {
+            TreeNode next = it.next();
+            if (it.hasNext()) {
+                next.print(buffer, childrenPrefix + "├── ", childrenPrefix + "│   ");
+            } else {
+                next.print(buffer, childrenPrefix + "└── ", childrenPrefix + "    ");
+            }
+        }
+    }
+}
