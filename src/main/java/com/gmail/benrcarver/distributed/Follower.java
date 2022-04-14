c14fc459dd32c3ba803c2b6af7477f80ac0472b6
Scusemua <benrcarver@gmail.com>
2022-04-14 16:17:04 -0400

    Added some prints

--- src/main/java/com/gmail/benrcarver/distributed/Follower.java
+++ src/main/java/com/gmail/benrcarver/distributed/Follower.java
@@ -44,20 +44,16 @@ public class Follower {
     private Configuration hdfsConfiguration;
     private DistributedFileSystem hdfs;
 
-    // TODO: Make it so we can change these dynamically.
-    private String serverlessLogLevel = "INFO";
-    private boolean consistencyEnabled = true;
-
     public synchronized void waitUntilDone() throws InterruptedException {
         this.wait();
     }
 
-    public Follower(String masterIp, int masterPort, String serverlessLogLevel, boolean disableConsistency) {
+    public Follower(String masterIp, int masterPort) {
         client = new Client(16000, 16000);
         this.masterIp = masterIp;
         this.masterPort = masterPort;
-        this.serverlessLogLevel = serverlessLogLevel;
-        this.consistencyEnabled = !disableConsistency;
+
+        LOG.debug("Follower being created. Commander IP: " + masterIp + ", commander port: " + masterPort);
 
         Commands.IS_FOLLOWER = true;
 
@@ -313,6 +309,8 @@ public class Follower {
 
         Thread connectThread = new Thread(() -> {
             client.start();
+
+            LOG.debug("Trying to connect to Commander at " + masterIp + ":" + masterPort + ".");
 
             try {
                 client.connect(CONN_TIMEOUT_MILLISECONDS, masterIp, masterPort, masterPort + 1);
--- src/main/java/com/gmail/benrcarver/distributed/InteractiveTest.java
+++ src/main/java/com/gmail/benrcarver/distributed/InteractiveTest.java
@@ -24,8 +24,6 @@ public class InteractiveTest {
         Option leaderIpOpt = new Option("l", "leader_ip", true, "The IP address of the Leader. Only used when this process is designated as a worker.");
         Option leaderPort = new Option("p", "leader_port", true, "The port of the Leader. Only used when this process is designated as a worker.");
         Option localOption = new Option("n", "nondistributed", false, "Run in non-distributed mode, meaning we don't launch any followers.");
-        Option logLevelOption = new Option("ll", "loglevel", true, "The log4j log level to pass to the NameNodes.");
-        Option consistencyProtocolOption = new Option("c", "disable_consistency", false, "If passed, then we default to disabling the consistency protocol.");
 
         Option yamlPath = new Option("y", "yaml_path", true, "Path to YAML configuration file.");
 
@@ -34,8 +32,6 @@ public class InteractiveTest {
         cmdLineOpts.addOption(leaderPort);
         cmdLineOpts.addOption(yamlPath);
         cmdLineOpts.addOption(localOption);
-        cmdLineOpts.addOption(logLevelOption);
-        cmdLineOpts.addOption(consistencyProtocolOption);
 
         CommandLineParser parser = new GnuParser();
         HelpFormatter formatter = new HelpFormatter();
@@ -57,9 +53,7 @@ public class InteractiveTest {
             LOG.info("Beginning execution as FOLLOWER now.");
             Follower follower = new Follower(
                     cmd.getOptionValue("leader_ip"),
-                    Integer.parseInt(cmd.getOptionValue("leader_port")),
-                    cmd.hasOption("loglevel") ? cmd.getOptionValue("loglevel") : "DEBUG",
-                    cmd.hasOption("disable_consistency") /* If it has this option, then it is true */);
+                    Integer.parseInt(cmd.getOptionValue("leader_port")));
             follower.connect(); // Connect to the Commander.
             follower.waitUntilDone(); // Basically run forever.
         } else {
