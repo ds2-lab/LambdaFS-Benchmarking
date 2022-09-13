5c38c2519f7f57ee3a265173775ae40a2923e582
Scusemua <benrcarver@gmail.com>
2022-09-13 16:20:53 -0400

    ListDirectory always prints number of files in directory.

--- src/main/java/com/gmail/benrcarver/distributed/Commands.java
+++ src/main/java/com/gmail/benrcarver/distributed/Commands.java
@@ -1132,6 +1132,8 @@ public class Commands {
                 for (FileStatus status : fileStatus)
                     LOG.debug(status.getPath().toString());
                 LOG.debug("Directory '" + targetDirectory + "' contains " + fileStatus.length + " files.");
+            } else {
+                LOG.info("Number of files in directory: " + fileStatus.length);
             }
             return true;
         } catch (IOException ex) {
