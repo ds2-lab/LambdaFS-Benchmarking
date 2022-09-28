b3aabfd0bf3c7023fb652ab5c98362798702e706
Scusemua <benrcarver@gmail.com>
2022-09-28 14:09:31 -0400

    Fixed yet another bug that occurs when HTTP latency is zero

--- src/main/java/com/gmail/benrcarver/distributed/Commander.java
+++ src/main/java/com/gmail/benrcarver/distributed/Commander.java
@@ -1870,6 +1870,20 @@ public class Commander {
         int totalCacheHits = (int)(cacheHits.getN() > 0 ? cacheHits.getSum() : 0);
         int totalCacheMisses = (int)(cacheMisses.getN() > 0 ? cacheMisses.getSum() : 0);
 
+        double combinedLatency = 0.0;
+        int divisor = 0;
+        if (trialAvgTcpLatency > 0) {
+            combinedLatency += trialAvgTcpLatency;
+            divisor++;
+        }
+        if (trialAvgHttpLatency > 0) {
+            combinedLatency += trialAvgHttpLatency;
+            divisor++;
+        }
+
+        if (divisor > 0)
+            combinedLatency /= divisor;
+
         double cacheHitRate = 0.0;
         if (totalCacheHits > 0 || totalCacheMisses > 0)
             cacheHitRate = ((double)totalCacheHits / ((double)totalCacheHits + (double)totalCacheMisses));
@@ -1882,14 +1896,14 @@ public class Commander {
         LOG.info("Cache hit percentage: " + cacheHitRate);
         LOG.info("Average TCP latency: " + trialAvgTcpLatency + " ms");
         LOG.info("Average HTTP latency: " + trialAvgHttpLatency + " ms");
-        LOG.info("Average combined latency: " + (trialAvgTcpLatency + trialAvgHttpLatency) / 2.0 + " ms");
+        LOG.info("Average combined latency: " + combinedLatency + " ms");
         LOG.info("Aggregate Throughput (ops/sec): " + aggregateThroughput);
 
         DecimalFormat df = new DecimalFormat("#.####");
         String metricsString = String.format("%s %d %d %s %s %s %s", df.format(aggregateThroughput),
                 totalCacheHits, totalCacheMisses,
                 df.format(cacheHitRate), df.format(trialAvgTcpLatency),
-                df.format(trialAvgHttpLatency), df.format((trialAvgTcpLatency + trialAvgHttpLatency) / 2.0));
+                df.format(trialAvgHttpLatency), df.format(combinedLatency));
 
         LOG.info(metricsString);
 
