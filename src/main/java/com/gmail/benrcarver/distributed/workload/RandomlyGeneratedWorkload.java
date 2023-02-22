1f42b24d6745c9aedf496effc4534bc741a71845
Scusemua <benrcarver@gmail.com>
2023-02-22 16:32:37 -0500

    Wait longer for workload to finish.

--- src/main/java/com/gmail/benrcarver/distributed/workload/RandomlyGeneratedWorkload.java
+++ src/main/java/com/gmail/benrcarver/distributed/workload/RandomlyGeneratedWorkload.java
@@ -239,7 +239,7 @@ public class RandomlyGeneratedWorkload {
 
         boolean acquired = false;
         try {
-            acquired = endSemaphore.tryAcquire((long)(duration * 1.3), TimeUnit.MILLISECONDS);
+            acquired = endSemaphore.tryAcquire(duration * 2, TimeUnit.MILLISECONDS);
         } catch (InterruptedException ex) {
             LOG.error("Interrupted while waiting for workload to complete:", ex);
         }
