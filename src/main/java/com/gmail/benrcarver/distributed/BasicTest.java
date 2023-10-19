88123bbdf7bb0eeef5470af69ab243a0a9378155
Scusemua <benrcarver@gmail.com>
2023-10-19 10:22:33 -0400

    Updated BasicTest.java

--- src/main/java/com/gmail/benrcarver/distributed/BasicTest.java
+++ src/main/java/com/gmail/benrcarver/distributed/BasicTest.java
@@ -76,9 +76,109 @@ public class BasicTest {
             System.out.println("[SUCCESS] Everything appears to be working!");
         }
 
-        hdfs.close();
+        System.out.println("\nCreating subdirectory \"/my_directory\" now.");
+
+        boolean success = Commands.mkdir("/my_directory", hdfs);
+
+        if (!success) {
+            System.out.println("Something went wrong when creating directory \"/my_directory\"...");
+            System.exit(1);
+        }
+
+        System.out.println("Created directory \"/my_directory\". Let's see if it shows up when we list the contents of the root directory \"/\"...\n");
+
+        System.out.println("Contents of directory \"/\":\n");
+        fileStatus = hdfs.listStatus(new Path(nameNodeEndpoint + "/"));
+        for (FileStatus status : fileStatus)
+            System.out.println(status.getPath().toString());
+        System.out.println("\nDirectory \"/\" contains " + fileStatus.length + " files.\n");
+
+        if (fileStatus.length != 2) {
+            System.out.println("[ERROR] The directory should contain 2 files now, not " + fileStatus.length + ".");
+            System.out.println("Something is going wrong!");
+        } else {
+            System.out.println("[SUCCESS] Everything appears to be working!");
+        }
+
+        System.out.println("\nLet's create some files in the /my_directory folder.");
+
+        int numFiles;
+        while (true) {
+            numFiles = Commands.getIntFromUser("How many files should we create? [1 - 100]");
+
+            if (numFiles <= 0) {
+                System.out.println("\nPlease enter a positive value between 1 and 100. You entered: \"" + numFiles +"\"");
+            }
+            else if (numFiles > 100) {
+                System.out.println("Please enter a value between 1 and 100. You entered: \"" + numFiles + "\"");
+            }
+            else {
+                System.out.println("\nGot it! We'll create " + numFiles + " file(s) in the \"/my_directory\" directory.");
+                break;
+            }
+        }
+
+        for (int i = 0; i < numFiles; i++) {
+            String filePath = "/my_directory/my_file" + i + ".txt";
+            System.out.println("Creating file: \"" + filePath + "\"");
+            success = Commands.createFile(filePath, "", hdfs);
+
+            if (!success) {
+                System.out.println("Something went wrong when creating file \"" + filePath + "\"");
+                System.exit(1);
+            }
+        }
+
+        System.out.println("\nListing the contents of the \"/my_directory\" directory now.");
+        System.out.println("Contents of directory \"/my_directory/\":\n");
+        fileStatus = hdfs.listStatus(new Path(nameNodeEndpoint + "/my_directory"));
+        for (FileStatus status : fileStatus)
+            System.out.println(status.getPath().toString());
+        System.out.println("\nDirectory \"/my_directory\" contains " + fileStatus.length + " files.\n");
+
+        if (fileStatus.length != numFiles) {
+            System.out.println("[ERROR] The directory should contain " + numFiles + " file(s), not " + fileStatus.length + ".");
+            System.out.println("Something is going wrong!");
+        } else {
+            System.out.println("[SUCCESS] Everything appears to be working!");
+        }
+
+        System.out.println("Now let's try to delete everything that we just created, starting with \"test.txt\"");
+
+        success = Commands.delete("test.txt", hdfs);
+
+        if (!success) {
+            System.out.println("Error while deleting \"test.txt\"...");
+            System.exit(1);
+        }
+
+        System.out.println("Successfully deleted \"test.txt\". Next, let's delete \"/my_directory\"");
+
+        success = Commands.delete("/my_directory", hdfs);
+
+        if (!success) {
+            System.out.println("Error while deleting \"/my_directory\"...");
+            System.exit(1);
+        }
+
+        System.out.println("Let's list the contents of the root directory \"/\" again and see if it's empty.");
+
+        System.out.println("Contents of directory \"/\":\n");
+        fileStatus = hdfs.listStatus(new Path(nameNodeEndpoint + "/"));
+        for (FileStatus status : fileStatus)
+            System.out.println(status.getPath().toString());
+        System.out.println("\nDirectory \"/\" contains " + fileStatus.length + " files.\n");
+
+        if (fileStatus.length != 0) {
+            System.out.println("[ERROR] The directory should contain no files now, not " + fileStatus.length + ".");
+            System.out.println("Something is going wrong!");
+        } else {
+            System.out.println("[SUCCESS] Everything appears to be working!");
+        }
 
         System.out.println("Exiting.");
+
+        hdfs.close();
 
         System.exit(0);
     }
